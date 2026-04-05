0d5a1e03ee35601e1df8bb89311e14ad2eb7ad88
Rob Coe <robcoe18@gmail.com>
2026-04-04 20:01:26 -0700

    Initial commit
    copied files from Weather Monitor

---
+++ ReadSDCard.py
@@ -0,0 +1,45 @@
+###############  This program will read the index file and print the lastest data run
+
+
+import os
+import board
+import busio as io
+import digitalio
+import storage
+import adafruit_sdcard
+#import microcontroller
+from time import sleep
+# Use any pin that is not taken by SPI
+SD_CS = board.GP17
+ 
+# Connect to the card and mount the filesystem.
+spi = io.SPI(board.GP18, board.GP19, board.GP16)
+cs = digitalio.DigitalInOut(SD_CS)
+sdcard = adafruit_sdcard.SDCard(spi, cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd") 
+
+
+"""with open("/sd/indexfile.txt", "w") as writefile:        # Uncomment this section to start a new index file from 1
+    print(str(1), file=writefile)
+    writefile.close()
+"""
+
+with open("/sd/indexfile.txt", "r") as inputfile:
+    for line in inputfile:
+        i= line
+        #print(i)
+    inputfile.close
+    index = int(i[0])
+    print("\nindex =",index)
+file_name = "/sd/history"+str(index)+".txt"
+print("\nfilename:", file_name)
+
+#while True:
+
+with open(file_name, "r") as inputfile:      
+    for line in inputfile:
+        print(line)
+    inputfile.close    
+
+print("stopped")
---
+++ code.py
@@ -0,0 +1,7 @@
+#import WeatherMonitorTest
+#import Blink_Led
+ 
+import WeatherMonitorSave
+
+#import SDCardTest
+import ReadSDCard
